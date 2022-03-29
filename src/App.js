f2fecaa30eef1dac716cccaee21536c1a64dfc1a
Adlai M.B <adlaimb@gmail.com>
2022-03-28 23:43:36 -0700

    create function to intiate the game by saving the word

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import Input from './Input';
 
 function App() {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const word = window.localStorage.getItem("word");
+
+  // check for a non empty word and update playing state if we have a word
+  const saveWord = (word) => {
+
+    if (word.length > 0){
+
+      window.localStorage.setItem("word", word);
+      setIsPlaying(true);
+
+    } else { 
+
+      setIsPlaying(false);
+
+    }
+
+  };
+
   return (
     <div className="App">
       <h1>_HangMan_</h1>
-      <Input/>
+      {!isPlaying ? <Input saveWord={saveWord}/> : <span>{word}</span>}
     </div>
   );
 }
--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,28 @@
-function Input() {
+import { useState } from "react";
+
+function Input({saveWord}) {
+
+    const [word, setWord] = useState("");
+
+    // update the word whenever the input is changed
+    const updateWord = (event) => {
+
+        const change = event.target.value;
+
+        setWord(change);
+
+    };
+
+    // save the word when the button is pressed and start the game
+    const startGame = () => {
+        saveWord(word)
+    }
+
     return (
-        <input type="text" minlenght="1" placeholder="Enter a word..." required/>
+        <>
+        <input onChange={updateWord} type="text" minlenght="1" placeholder="Enter a word..." required/>
+        <button onClick={startGame}>Start Game</button>
+        </>
     );
 }
 
